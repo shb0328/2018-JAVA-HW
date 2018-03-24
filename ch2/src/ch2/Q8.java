16054ed7a71513024632a2935c57af84f33e1ba7
shb0328 <hyebeen0328@naver.com>
2018-03-24 22:46:28 +0900

    logic error /  주석처리

--- ch2/src/ch2/Q8.java
+++ ch2/src/ch2/Q8.java
@@ -22,15 +22,15 @@ public class Q8 {
 		int y2 = scanner.nextInt();
 		
 		//검사 1) 사각형이 기존 사각형을 완전히 포함하지 않고 곂치는 경우
-		boolean case1 = (rectx1<=x2 && x2<=rectx2) && (recty1<=y2 && y2<=recty2) ;
-		boolean case2 = (rectx1<=x2 && x2<=rectx2) && (recty1<=y1 && y1<=recty2) ;
-		boolean case3 = (rectx1<=x1 && x1<=rectx2) && (recty1<=y1 && y1<=recty2) ;
-		boolean case4 = (rectx1<=x1 && x1<=rectx2) && (recty1<=y2 && y2<=recty2) ;
+		//boolean case1 = (rectx1<=x2 && x2<=rectx2) && (recty1<=y2 && y2<=recty2) ;//100<x2<200,100<y2<200
+		//boolean case2 = (rectx1<=x2 && x2<=rectx2) && (recty1<=y1 && y1<=recty2) ;//100<x2<200,100<y1<200
+		//boolean case3 = (rectx1<=x1 && x1<=rectx2) && (recty1<=y1 && y1<=recty2) ;//100<x1<200,100<y1<200
+		//boolean case4 = (rectx1<=x1 && x1<=rectx2) && (recty1<=y2 && y2<=recty2) ;//100<x1<200,100<y2<200
 		
-		//TODO : 검사 2) 사각형이 기존 사각형을 완전히 포함하는 경우
-		boolean specialCase = ();
+		// 검사 2) 사각형이 기존 사각형을 완전히 포함하는 경우
+		//boolean specialCase = (x1<=rectx1 && y1 <=recty1 && x2 >= rectx2 && y2 >= recty2);
 		
-		if(case1 || case2 || case3 || case4)
+		if(/*case1 || case2 || case3 || case4 || specialCase*/)
 			System.out.print("충돌합니다.");
 		else  System.out.print("충돌하지 않습니다.");
 		
