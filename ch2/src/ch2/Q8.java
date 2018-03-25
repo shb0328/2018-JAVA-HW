0d2495947efffc3f3747238243f58636a6c78822
shb0328 <hyebeen0328@naver.com>
2018-03-25 11:21:38 +0900

    함수화 : inRect()

--- ch2/src/ch2/Q8.java
+++ ch2/src/ch2/Q8.java
@@ -21,34 +21,27 @@ public class Q8 {
 		int x2 = scanner.nextInt();
 		int y2 = scanner.nextInt();
 		
-		/*=========망할 십자가 모양 부분과 곂친다고 판단=========
-		//검사 1)사각형이 기존 사각형보다 작거나 같은 경우
-		boolean case1 = (rectx1<=x1 && x1<=rectx2) && (recty1<=y1 && y1<=recty2) ;//100<x1<200,100<y1<200
-		boolean case2 = (rectx1<=x2 && x2<=rectx2) && (recty1<=y1 && y1<=recty2) ;//100<x2<200,100<y1<200
-			
-		//검사 2)사각형이 기존 사각형보다 큰 경우
-		boolean case3 = (x1<=rectx1 && rectx1<=x2) && (y1<=recty1 && recty1<=y2) ;//x1<100<x2,y1<100<y2
-		boolean case4 = (x1<=rectx2 && rectx2<=x2) && (y1<=recty2 && recty2<=y2) ;//x1<200<x2,y1<200<y2
-		*/
+		boolean result = inRect(x1,y1,x2,y2,rectx1,recty1,rectx2, recty2);
 		
-		//error 수정 : 망할 십자가모양 = 완전포함 나머지 전부 커버가능
-		boolean case5 = ((rectx1<=x1 && x1<=rectx2) || (rectx1<=x2 && x2<=rectx2)) 
-						&& ((y1<=recty1 && recty1<=y2) || (y1<=recty2 && recty2<=y2));
-		boolean case6 = ((x1<=rectx1 && rectx1<=x2) || (x1<=rectx2 && rectx2<=x2) ) 
-						&& ((recty1<=y1 && y1<=recty2) || (recty1<=y2 && y2<=recty2));
-		//완전 포함 관계일 경우
-		boolean case7 = ((rectx1>x1 && x2>rectx2) && (recty1>y1 && y2>recty2)); //사각형이 기존사각형을 완전포함
-		boolean case8 = ((rectx1<x1 && x2<rectx2) && (recty1<y1 && y2<recty2)); //기존사각형이 사각형을 완전포함
-		
-		if(/*case1 || case2 || case3 || case4 ||*/ case5 || case6 || case7 || case8)
+		if(result)
 			System.out.print("충돌합니다.");
 		else  System.out.print("충돌하지 않습니다.");
 		
 		scanner.close();
 	}
 
+	public static boolean inRect(int x1, int y1, int x2, int y2, int rectx1, int recty1, int rectx2, int recty2) { //충돌검사함수
+		//error 수정 : 망할 십자가모양 = 완전포함 나머지 전부 커버가능
+		boolean case1 = ((rectx1<=x1 && x1<=rectx2) || (rectx1<=x2 && x2<=rectx2)) 
+						&& ((y1<=recty1 && recty1<=y2) || (y1<=recty2 && recty2<=y2));
+		boolean case2 = ((x1<=rectx1 && rectx1<=x2) || (x1<=rectx2 && rectx2<=x2) ) 
+						&& ((recty1<=y1 && y1<=recty2) || (recty1<=y2 && y2<=recty2));
+		//완전 포함 관계일 경우
+		boolean case3 = ((rectx1>x1 && x2>rectx2) && (recty1>y1 && y2>recty2)); //사각형이 기존사각형을 완전포함
+		boolean case4 = ((rectx1<x1 && x2<rectx2) && (recty1<y1 && y2<recty2)); //기존사각형이 사각형을 완전포함
+		
+		if( case1 || case2 || case3 || case4)
+			return true;
+		else  return false;	
 }
-
-//public static boolean inRect(int x1, int y1, int x2, int y2, int rectx1,) { //
-	
-//}
+}
