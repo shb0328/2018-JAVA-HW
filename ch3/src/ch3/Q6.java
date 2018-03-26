3fc39c8857d3d1aad085da8707149341af672b37
shb0328 <hyebeen0328@naver.com>
2018-03-27 00:14:23 +0900

    돈 분류 프로그램

---
+++ ch3/src/ch3/Q6.java
@@ -0,0 +1,21 @@
+package ch3;
+
+import java.util.Scanner;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		int[] unit = {50000, 10000, 1000, 500, 100, 50 , 10, 1}; //환산할 돈의 종류
+		
+		System.out.print("금액을 입력하시오>>");
+		Scanner scanner = new Scanner(System.in);
+		int money = scanner.nextInt();
+		for(int u : unit) {
+			System.out.print(u+"원 짜리 : "+ money/u +"개");
+			money = money - ((money/u)*u);
+		}
+		
+
+	}
+
+}
