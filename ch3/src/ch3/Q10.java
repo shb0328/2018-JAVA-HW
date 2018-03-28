70e8c3065515dfdc5bbaa2280f0dbc4708f26693
shb0328 <hyebeen0328@naver.com>
2018-03-29 00:42:17 +0900

    배열 크기 상수화

--- ch3/src/ch3/Q10.java
+++ ch3/src/ch3/Q10.java
@@ -1,13 +1,14 @@
 package ch3;
 
 public class Q10 {
+	final static int SIZE_OF_ARRAY = 4;
 	public static void main(String[] args) {
-		int[][] num = new int[4][4];
+		int[][] num = new int[SIZE_OF_ARRAY][SIZE_OF_ARRAY];
 		int firstIndex,secondIndex;
 				
 		for(int i=0;i<10;++i) { 
-			firstIndex=(int)(Math.random()*100%4);//0~3 랜덤 index
-			secondIndex=(int)(Math.random()*100%4);//0~3 랜덤 index
+			firstIndex=(int)(Math.random()*100%num.length);//0~3 랜덤 index
+			secondIndex=(int)(Math.random()*100%num[firstIndex].length);//0~3 랜덤 index
 			if(num[firstIndex][secondIndex] != 0) //이미 값이 채워져있다면
 				--i; //다시
 			else
