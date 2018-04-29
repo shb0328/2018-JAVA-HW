44f427379a13498c8910036bb12018523ca30128
shb0328 <hyebeen0328@naver.com>
2018-04-29 22:49:21 +0900

    BOJ11718

---
+++ baekjoon/src/BOJ11718.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ11718 {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		while (scr.hasNextLine()) {
+			System.out.println(scr.nextLine());
+		}
+
+		scr.close();		
+	}
+
+}
+
