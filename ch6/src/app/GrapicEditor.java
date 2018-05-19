fa5e78385b66c1226c7253d81bfc10b266e8ab3a
shb0328 <hyebeen0328@naver.com>
2018-05-19 22:39:15 +0900

    init ch6 Q4

---
+++ ch6/src/app/GrapicEditor.java
@@ -0,0 +1,14 @@
+package app;
+
+import base.Shape;
+import derived.Circle;
+
+public class GrapicEditor {
+
+	public static void main(String[] args) {
+		Shape shape = new Circle();
+		shape.draw();
+
+	}
+
+}
---
+++ ch6/src/derived/Circle.java
@@ -0,0 +1,8 @@
+package derived;
+
+import base.Shape;
+
+public class Circle extends Shape{
+	@Override
+	public void draw() {System.out.println("Circle");}
+}
